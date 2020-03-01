642078b51510df0995db685f5d891d9f73769f80
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-01 14:27:48 +0700

    Update head.blade.php

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -5,22 +5,22 @@
     
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/open-iconic-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/animate.css')}}">
     
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/magnific-popup.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/aos.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/ionicons.min.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
-    <link rel="stylesheet" href="{{asset('css/jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/jquery.timepicker.css')}}">
 
     
-    <link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
-    <link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
   </head>
