68220fbd13b55e38f3d88a4aca3c019f4852fd4e
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-06 16:16:17 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
             'username' => "ipymoyzaqrhxoh", //env('DB_USERNAME', 'forge'),
             'password' =>
                 "37c9b44567ddf272e5173c548be24afec5783636dbd5feac2acc5413adfd716a", //env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
