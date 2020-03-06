b4531107de373b83b9400f09e0187b1ae79756b3
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-06 12:31:21 +0700

    update

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -15,16 +15,14 @@ class WebController extends Controller
     public function postClassRoom(Request $request)
     {
         $student = [
-
             "name" => $request->get("input-name"),
             "address" => $request->get("input-address"),
             "email" => $request->get("input-email"),
             "phone" => $request->get("input-phone")
-
         ];
 
         array_push($_SESSION, $student);
-        //dd($_SESSION);   
+        //dd($_SESSION);
         return view("student_listing", ["student" => $_SESSION]);
     }
     public function getClassRoom()
@@ -39,34 +37,62 @@ class WebController extends Controller
     }
 
     public function homePage()
-    {        
-        $product1 = Product::take(8)->join("category","category.id","=","product.category_id")
-        ->orderBy("price","asc")->select('product.*', 'category.id as cate_id')->get();
-        $product3 = Product::take(8)->join("category","category.id","=","product.category_id")
-        ->orderBy("product.created_at","asc")->select('product.*', 'category.id as cate_id')->get();
-        $product2 = Product::take(8)->join("category","category.id","=","product.category_id")
-        ->orderBy("price","desc")->select('product.*', 'category.id as cate_id')->get();
-        return view('home',["product1"=>$product1,"product2"=>$product2,"product3"=>$product3]);
+    {
+        $product1 = Product::take(8)
+            ->join("category", "category.id", "=", "product.category_id")
+            ->orderBy("price", "asc")
+            ->select('product.*', 'category.id as cate_id')
+            ->get();
+        $product3 = Product::take(8)
+            ->join("category", "category.id", "=", "product.category_id")
+            ->orderBy("product.created_at", "asc")
+            ->select('product.*', 'category.id as cate_id')
+            ->get();
+        $product2 = Product::take(8)
+            ->join("category", "category.id", "=", "product.category_id")
+            ->orderBy("price", "desc")
+            ->select('product.*', 'category.id as cate_id')
+            ->get();
+        return view('home', [
+            "product1" => $product1,
+            "product2" => $product2,
+            "product3" => $product3
+        ]);
     }
     public function shopPage()
     {
         //$product = Product::take(8)->join("category","category.id","=","product.category_id")->where("category.id",5)->orderBy("product_name","asc")->get();
-        $product = Product::take(8)->where("product.category_id",5)->orderBy("product_name","asc")->get();
-        
-        return view('shop',["product"=>$product]);
+        $product = Product::take(8)
+            ->where("product.category_id", 5)
+            ->orderBy("product_name", "asc")
+            ->get();
+
+        return view('shop', ["product" => $product]);
     }
 
     public function singlePage($id)
     {
-        
         $mainProduct = Product::find($id);
-        $product_cate = Product::take(8)->join("category","category.id","=","product.category_id")
-        ->where("category.id",$mainProduct->category_id)
-        ->orderBy("product_name","asc")->select('product.*', 'category.id as cate_id')->get();
-        $product_brand = Product::take(8)->join("brand","brand.id","=","product.brand_id")
-        ->where("brand.id",$mainProduct->brand_id)
-        ->orderBy("product_name","asc")->select('product.*', 'brand.id as b_id')->get();
-        
-        return view('single',["product_cate"=>$product_cate,"product_brand"=>$product_brand],["mainProduct"=>$mainProduct]);
+        $product_cate = Product::take(8)
+            ->join("category", "category.id", "=", "product.category_id")
+            ->where("category.id", $mainProduct->category_id)
+            ->orderBy("product_name", "asc")
+            ->select('product.*', 'category.id as cate_id')
+            ->get();
+        $product_brand = Product::take(8)
+            ->join("brand", "brand.id", "=", "product.brand_id")
+            ->where("brand.id", $mainProduct->brand_id)
+            ->orderBy("product_name", "asc")
+            ->select('product.*', 'brand.id as b_id')
+            ->get();
+
+        return view(
+            'single',
+            [
+                "product_cate" => $product_cate,
+                "product_brand" => $product_brand
+            ],
+            ["mainProduct" => $mainProduct]
+        );
     }
 }
