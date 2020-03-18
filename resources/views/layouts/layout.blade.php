83286f5246a76c8a77ff36327dd5087254e9fb08
jklhdd <49911153+jklhdd@users.noreply.github.com>
2020-03-18 10:47:24 +0700

    update

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,11 +12,11 @@
   <title>@yield('title')</title>
 
   <!-- Custom fonts for this template-->
-  <link href="{{asset('admin/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('admin/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('admin/css/sb-admin-2.css')}}" rel="stylesheet"/>
 
 </head>
 
